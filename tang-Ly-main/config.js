c726e9dd9d5f1dbb50b660fbbd0ad2c86f6c06e1
bac1807 <65804384+bac1807@users.noreply.github.com>
2021-12-30 20:42:25 +0700

    Add files via upload

---
+++ tang-Ly-main/config.js
@@ -0,0 +1,30 @@
+const CONFIG = {
+    introTitle: ' Ly Yêu của anh à!',
+    introDesc: ` Anh biết Là hai đứa Mình 
+                 Không thể đi xa hơn với nhau được nữa
+                 Những anh Yêu Em rất nhiều
+                 Cho dù sau này có xa nhau
+                 Mong em Đừng có ghét Anh
+                 Anh làm cái này để 
+                 Sau này mình có thể xem lại
+                 Mặc dù không thể che trở cho em
+                 Hay là làm cho em vui như những
+                 Người đàn ông khác
+                 Nhưng mà anh thì lúc nào cũng Yêu Khánh Ly
+                 Một cái tên rất đẹp
+                 Anh Luôn Yêu Em <3   `,
+          
+
+    btnIntro: 'hihi',
+    title: 'Anh yêu em nhiều lắm',
+    desc: 'Hãy ở Bên nhau Lâu nhất có thể và đừng Suy nghĩ nhiều nhé ',
+    btnYes: 'Yêu anh Nhiều <3',
+    btnNo: 'Không :((',
+    question:'Trên thế giới hơn 7 tỉ người mà em là người đẹp nhất <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Yêu thương công chúa của anh.',
+    btnAccept: 'Thương',
+    messLink: 'https://www.facebook.com/nguyenphuong.bac.585/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
